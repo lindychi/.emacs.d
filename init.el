ff09607310564f6d6175bed56a13a932d4908b59
hanchi <igam0000@gmail.com>
2018-02-22 19:13:00 +0900

    korean update

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(set-language-environment "Korean")
+(prefer-coding-system 'utf-8)
+
 (setq inhibit-startup-message t)
 
 (require 'package)
@@ -11,3 +14,16 @@
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit projectile))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
